a4a982dc36b35dec94b146b11a58f9c24ccecea2
sergiomm50 <sergiomm50@gmail.com>
2025-03-08 19:08:13 +0100

    feat: Add MuscularGroup enum for muscle categorization
    - Defines `MuscularGroup` enum to classify muscles into groups.
    - Includes categories: CHEST, BACK, SHOULDERS, LEGS, and ABDOMEN.

---
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/enums/MuscularGroup.java
@@ -0,0 +1,15 @@
+package com.returdev.catalog_service.enums;
+
+/**
+ * Enum representing different muscular groups in the system.
+ * This enum is used to categorize muscles into specific groups such as chest, back, shoulders, legs, and abdomen.
+ */
+public enum MuscularGroup {
+
+    CHEST,
+    BACK,
+    SHOULDERS,
+    LEGS,
+    ABDOMEN;
+
+}
