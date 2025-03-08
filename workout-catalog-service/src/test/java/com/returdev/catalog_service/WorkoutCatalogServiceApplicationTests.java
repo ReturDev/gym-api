d370c47125943d3a1c37e1909a904d6b63c5a085
sergiomm50 <sergiomm50@gmail.com>
2025-03-08 17:35:54 +0100

    Add workout-catalog-service to manage workouts and equipment

---
+++ workout-catalog-service/src/test/java/com/returdev/catalog_service/WorkoutCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.returdev.catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
