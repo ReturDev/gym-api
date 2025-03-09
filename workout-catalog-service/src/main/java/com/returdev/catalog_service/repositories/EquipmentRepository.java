0398692eb0cf6034b53d3a075ebca95cf5368e17
sergiomm50 <sergiomm50@gmail.com>
2025-03-09 17:02:21 +0100

    fix: Add @Modifying and @Transactional to update queries in EquipmentRepository
    - Fixed missing `@Modifying` and `@Transactional` annotations on update queries.
    - Ensures proper execution of update operations in `updateEquipmentName` and `updateEquipmentImageUrl`.
    - Resolves errors related to executing update queries in JPA.

--- workout-catalog-service/src/main/java/com/returdev/catalog_service/repositories/EquipmentRepository.java
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/repositories/EquipmentRepository.java
@@ -1,7 +1,9 @@
 package com.returdev.catalog_service.repositories;
 
 import com.returdev.catalog_service.entities.EquipmentEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -39,6 +41,8 @@ public interface EquipmentRepository extends JpaRepository<EquipmentEntity, Long
      * @param newName the new name to set
      * @return the number of entities updated (should be 1 if the update was successful)
      */
+    @Modifying
+    @Transactional
     @Query("UPDATE EquipmentEntity e SET e.name = :name WHERE e.id = :id")
     int updateEquipmentName(@Param("id") Long id, @Param("name") String newName);
 
@@ -49,6 +53,8 @@ public interface EquipmentRepository extends JpaRepository<EquipmentEntity, Long
      * @param imageUrl the new image URL to set
      * @return the number of entities updated (should be 1 if the update was successful)
      */
+    @Modifying
+    @Transactional
     @Query("UPDATE EquipmentEntity e SET e.imageUrl = :imageUrl WHERE e.id = :id")
     int updateEquipmentImageUrl(@Param("id") Long id, @Param("imageUrl") String imageUrl);
 
