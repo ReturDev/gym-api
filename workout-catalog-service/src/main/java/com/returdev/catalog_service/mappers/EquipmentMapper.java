889fc194f7925b25c4fe3dbac12207da556cc2e2
sergiomm50 <sergiomm50@gmail.com>
2025-03-09 18:25:11 +0100

    feat: Implement EquipmentMapper for EquipmentEntity and DTO conversion
    - Implements the ModelMapper interface for converting between EquipmentEntity and its DTOs (EquipmentRequestDTO and EquipmentResponseDTO).
    - Provides methods to map EquipmentEntity to EquipmentResponseDTO and EquipmentRequestDTO to EquipmentEntity.
    - Ensures seamless transformation between entity and DTO layers in the application.

---
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/mappers/EquipmentMapper.java
@@ -0,0 +1,43 @@
+package com.returdev.catalog_service.mappers;
+
+import com.returdev.catalog_service.dtos.equipment.EquipmentRequestDTO;
+import com.returdev.catalog_service.dtos.equipment.EquipmentResponseDTO;
+import com.returdev.catalog_service.entities.EquipmentEntity;
+import org.springframework.stereotype.Component;
+
+/**
+ * Mapper implementation for converting between EquipmentEntity and its DTOs.
+ */
+@Component
+public class EquipmentMapper implements ModelMapper<EquipmentResponseDTO, EquipmentRequestDTO, EquipmentEntity> {
+
+    /**
+     * Converts an EquipmentEntity to an EquipmentResponseDTO.
+     *
+     * @param entity the EquipmentEntity to convert
+     * @return the converted EquipmentResponseDTO
+     */
+    @Override
+    public EquipmentResponseDTO toResponseDto(EquipmentEntity entity) {
+        return new EquipmentResponseDTO(
+                entity.getId(),
+                entity.getName(),
+                entity.getImageUrl()
+        );
+    }
+
+    /**
+     * Converts an EquipmentRequestDTO to an EquipmentEntity.
+     *
+     * @param requestDto the EquipmentRequestDTO to convert
+     * @return the converted EquipmentEntity
+     */
+    @Override
+    public EquipmentEntity toEntity(EquipmentRequestDTO requestDto) {
+        return new EquipmentEntity(
+                requestDto.id(),
+                requestDto.name(),
+                requestDto.imageUrl()
+        );
+    }
+}
