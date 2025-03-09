8fbc9d13c4d651cc8c440fc3224fb5d3fe425f08
sergiomm50 <sergiomm50@gmail.com>
2025-03-09 17:14:57 +0100

    feat: Implement EquipmentServiceImpl for equipment management
    - Implements `EquipmentService` with CRUD operations for `EquipmentEntity`.
    - Includes validation and exception handling for entity existence.
    - Supports updating equipment name and image URL separately.
    - Ensures proper error handling for missing entities.

---
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/services/equipment/EquipmentServiceImpl.java
@@ -0,0 +1,106 @@
+package com.returdev.catalog_service.services.equipment;
+
+import com.returdev.catalog_service.entities.EquipmentEntity;
+import com.returdev.catalog_service.repositories.EquipmentRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * {@inheritDoc}
+ */
+@Service
+@RequiredArgsConstructor
+public class EquipmentServiceImpl implements EquipmentService {
+
+    private final EquipmentRepository equipmentRepository;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EquipmentEntity saveEquipment(EquipmentEntity equipment) throws IllegalArgumentException {
+        if (equipment.getId() != null) {
+            throw new IllegalArgumentException("Equipment ID must be null for a new entity");
+        }
+        return equipmentRepository.save(equipment);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<EquipmentEntity> getAllEquipments() {
+        return equipmentRepository.findAll();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EquipmentEntity getEquipmentById(Long id) {
+        return equipmentRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Equipment not found with ID: " + id));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public EquipmentEntity getEquipmentByName(String name) {
+        return equipmentRepository.findEquipmentByName(name).orElseThrow(() -> new EntityNotFoundException("Equipment not found with name: " + name));
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean existsByName(String name) {
+        return equipmentRepository.existsByName(name);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void deleteEquipmentById(Long id) {
+        equipmentRepository.deleteById(id);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void updateEquipment(EquipmentEntity equipment) {
+        existsById(equipment.getId());
+        equipmentRepository.save(equipment);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean updateEquipment(Long id, String name, String imageUrl) {
+        existsById(id);
+        if (name != null) {
+            return equipmentRepository.updateEquipmentName(id, name) == 1;
+        }
+        if (imageUrl != null) {
+            return equipmentRepository.updateEquipmentImageUrl(id, imageUrl) == 1;
+        }
+        return false;
+    }
+
+    /**
+     * Checks if an equipment entity with the given ID exists.
+     *
+     * @param id the ID of the equipment entity
+     * @throws EntityNotFoundException if no equipment entity with the specified ID is found
+     */
+    private void existsById(Long id) {
+        if (!equipmentRepository.existsById(id)) {
+            throw new EntityNotFoundException("Equipment not found with ID: " + id);
+        }
+    }
+}
