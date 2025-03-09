a0eecfbf3ce0082f648c9fc73c6c428d116bda21
sergiomm50 <sergiomm50@gmail.com>
2025-03-09 19:05:44 +0100

    fix: Add validation for 'name' and 'muscularGroup' fields in MuscleEntity
    - Added @Size and @NotNull validation to 'name' field to ensure it is not null and within the correct length.
    - Added @NotNull validation to 'muscularGroup' field to ensure it is not null.
    - Ensured proper validation consistency for both fields.

--- workout-catalog-service/src/main/java/com/returdev/catalog_service/entities/MuscleEntity.java
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/entities/MuscleEntity.java
@@ -2,6 +2,8 @@ package com.returdev.catalog_service.entities;
 
 import com.returdev.catalog_service.enums.MuscularGroup;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,11 +24,15 @@ public class MuscleEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @Size(min = 3, max = 25, message = "{validation.size.message}")
+    @NotNull(message = "{validation.not_null_required.message}")
     @Column(name = "name", unique = true)
     private String name;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "muscular_group")
+    @NotNull(message = "{validation.not_null_required.message}")
     private MuscularGroup muscularGroup;
 
 }
