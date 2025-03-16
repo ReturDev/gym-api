617c280cfe952d91db6a7332dab78b517763f5c3
sergiomm50 <sergiomm50@gmail.com>
2025-03-16 17:49:21 +0100

    feat: Add MuscleActivationLevel enum for categorizing muscle activation
    - Defines LOW, MEDIUM, and HIGH activation levels.

---
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/enums/MuscleActivationLevel.java
@@ -0,0 +1,13 @@
+package com.returdev.catalog_service.enums;
+
+/**
+ * Enum representing different levels of muscle activation.
+ * This enum is used to categorize the activation level of muscles involved in exercises.
+ */
+public enum MuscleActivationLevel {
+
+    LOW,
+    MEDIUM,
+    HIGH;
+
+}
