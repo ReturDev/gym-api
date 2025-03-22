009d73eea95b90ea1f7a4190eb9bfca31dced15e
sergiomm50 <sergiomm50@gmail.com>
2025-03-22 22:28:11 +0100

    feat: Add MuscleInvolvedResponseDTO for muscle involvement data transfer
    - Introduced `MuscleInvolvedResponseDTO` to represent muscles involved in an exercise.
    - Includes `id`, `muscle` details, and `activationLevel`.
    - Uses `@JsonProperty` for consistent JSON serialization.

---
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/dtos/muscle/involved/MuscleInvolvedResponseDTO.java
@@ -0,0 +1,19 @@
+package com.returdev.catalog_service.dtos.muscle.involved;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.returdev.catalog_service.dtos.muscle.MuscleRequestDTO;
+import com.returdev.catalog_service.enums.MuscleActivationLevel;
+
+/**
+ * Data Transfer Object (DTO) for representing a muscle involved in an exercise.
+ * This record is used to transfer data about the muscle and its activation level.
+ *
+ * @param id the unique identifier of the muscle involved
+ * @param muscle the muscle involved in the exercise
+ * @param activationLevel the level of activation of the muscle during the exercise
+ */
+public record MuscleInvolvedResponseDTO(
+        Long id,
+        MuscleRequestDTO muscle,
+        @JsonProperty("activation_level") MuscleActivationLevel activationLevel
+) {}
