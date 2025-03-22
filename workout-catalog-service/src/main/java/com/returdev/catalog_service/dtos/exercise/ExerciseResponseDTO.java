b52cace22c1f79193a044a8ac2cf80cd4a21b8be
sergiomm50 <sergiomm50@gmail.com>
2025-03-22 22:23:36 +0100

    feat: Add ExerciseResponseDTO for exercise data transfer
    - Introduced `ExerciseResponseDTO` to standardize exercise data representation.
    - Includes fields for exercise details, equipment, media URLs, and visibility status.
    - Uses `@JsonProperty` to ensure consistent JSON serialization.

---
+++ workout-catalog-service/src/main/java/com/returdev/catalog_service/dtos/exercise/ExerciseResponseDTO.java
@@ -0,0 +1,36 @@
+package com.returdev.catalog_service.dtos.exercise;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.returdev.catalog_service.dtos.equipment.EquipmentRequestDTO;
+import com.returdev.catalog_service.dtos.muscle.involved.MuscleInvolvedResponseDTO;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Data Transfer Object (DTO) for ExerciseEntity.
+ * This record is used to transfer exercise data between different layers of the application.
+ *
+ * @param id the unique identifier of the exercise
+ * @param name the name of the exercise
+ * @param description the description of the exercise
+ * @param benchRequired indicates if a bench is required for the exercise
+ * @param musclesInvolved the list of muscles involved in the exercise
+ * @param equipment the equipment required for the exercise
+ * @param imageUrl the URL of the image representing the exercise
+ * @param videoUrl the URL of the video demonstrating the exercise
+ * @param createdAt the date when the exercise was created
+ * @param isVisible indicates if the exercise is visible
+ */
+public record ExerciseResponseDTO(
+        Long id,
+        String name,
+        String description,
+        @JsonProperty("is_bench_required") boolean benchRequired,
+        @JsonProperty("muscles_involved") List<MuscleInvolvedResponseDTO> musclesInvolved,
+        EquipmentRequestDTO equipment,
+        @JsonProperty("image_url") String imageUrl,
+        @JsonProperty("video_url") String videoUrl,
+        @JsonProperty("created_at") Date createdAt,
+        @JsonProperty("is_visible") boolean isVisible
+) {}
