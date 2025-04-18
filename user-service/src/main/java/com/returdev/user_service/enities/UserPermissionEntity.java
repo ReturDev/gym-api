8c4c7f824c298aabf50139525bc33c59b464dfd4
sergiomm50 <sergiomm50@gmail.com>
2025-04-18 17:24:29 +0200

    feat: add UserPermissionEntity for managing user permissions
    - Introduced UserPermissionEntity class to represent user permissions in the system.
    - Mapped to "user_permissions" table with an auto-generated ID.
    - Defined a non-null, unique "permission" field to store permission types.
    - Used enum UserPermission to ensure consistent permission values.

---
+++ user-service/src/main/java/com/returdev/user_service/enities/UserPermissionEntity.java
@@ -0,0 +1,32 @@
+package com.returdev.user_service.enities;
+
+import com.returdev.user_service.enums.UserPermission;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Entity class representing a user permission in the system.
+ * This entity is mapped to the "user_permissions" table in the database.
+ */
+@Entity
+@Table(name = "user_permissions")
+public class UserPermissionEntity {
+
+    /**
+     * The unique identifier for the user permission.
+     * This value is auto-generated.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * The specific permission associated with this entity.
+     * This field is not nullable, cannot be updated, and must be unique.
+     */
+    @NotNull
+    @Column(name = "permission", updatable = false, nullable = false, unique = true)
+    @Enumerated(EnumType.STRING)
+    private UserPermission permission;
+
+}
