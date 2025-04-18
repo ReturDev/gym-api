7819faa5c626bb4e4eecbba16e8323dd0c92a314
sergiomm50 <sergiomm50@gmail.com>
2025-04-18 19:16:17 +0200

    feat: Add custom @ValidPassword annotation for password validation
    - Introduced a reusable annotation to enforce password validation logic using PasswordValidator.
    - Supports custom message, validation groups, and payload.

---
+++ user-service/src/main/java/com/returdev/user_service/annotations/validations/ValidPassword.java
@@ -0,0 +1,25 @@
+package com.returdev.user_service.annotations.validations;
+
+import com.returdev.user_service.annotations.validations.validators.PasswordValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+/**
+ * Custom annotation for validating passwords based on specific criteria.
+ * This annotation is validated by the `PasswordValidator` class.
+ */
+@Documented
+@Constraint(validatedBy = PasswordValidator.class)
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.PARAMETER})
+public @interface ValidPassword {
+
+    String message() default "{validation.ValidPassword.message}";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
