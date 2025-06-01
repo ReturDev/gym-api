dd92419cd56d8bbd9b3cc5f977ad654760ec0a40
sergiomm50 <sergiomm50@gmail.com>
2025-06-01 14:53:46 +0200

    feat: enable binding of keystore configuration properties
    - Annotated main application class with @EnableConfigurationProperties for KeystoreProperties
    - Allows automatic loading of keystore settings from application configuration

--- authentication-service/src/main/java/com/returdev/authentication_service/AuthenticationServiceApplication.java
+++ authentication-service/src/main/java/com/returdev/authentication_service/AuthenticationServiceApplication.java
@@ -1,9 +1,12 @@
 package com.returdev.authentication_service;
 
+import com.returdev.authentication_service.config.properties.KeystoreProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KeystoreProperties.class)
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
