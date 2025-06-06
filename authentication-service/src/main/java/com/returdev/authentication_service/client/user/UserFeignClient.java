39ae9cbaeea5eae4aa0018d708efaac380c68282
sergiomm50 <sergiomm50@gmail.com>
2025-06-06 22:00:04 +0200

    feat: add saveRefreshToken method to UserFeignClient
    - Introduced `saveRefreshToken(String token)` to allow saving user refresh tokens via Feign calls.

--- authentication-service/src/main/java/com/returdev/authentication_service/client/user/UserFeignClient.java
+++ authentication-service/src/main/java/com/returdev/authentication_service/client/user/UserFeignClient.java
@@ -5,16 +5,25 @@ import com.returdev.authentication_service.client.user.model.UserClientModel;
 
 /**
  * Feign client interface for interacting with the user service.
- * Provides methods for retrieving user information.
+ * Provides methods for retrieving user information and saving refresh tokens.
  */
 public interface UserFeignClient {
 
     /**
      * Retrieves a user by their email address.
+     * This method is used to fetch user details from the user service.
      *
      * @param email The email address of the user to retrieve.
      * @return A {@link UserClientModel} representing the user.
      */
     UserClientModel getUserByEmail(String email);
+
+    /**
+     * Saves a refresh token for a user.
+     * This method is used to store the refresh token in the user service.
+     *
+     * @param token The refresh token to save.
+     */
+    void saveRefreshToken(String token);
 
 }
