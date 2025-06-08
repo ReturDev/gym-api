7ef9699c869e2d14abb307c85cd1f7c29489a53e
sergiomm50 <sergiomm50@gmail.com>
2025-06-08 11:34:18 +0200

    refactor: update logout method and add token refresh in AuthService
    - Change logout method to accept refreshToken instead of email for better token invalidation.
    - Add provideNewAccessToken method to generate new access tokens using a refresh token.

--- authentication-service/src/main/java/com/returdev/authentication_service/services/auth/AuthService.java
+++ authentication-service/src/main/java/com/returdev/authentication_service/services/auth/AuthService.java
@@ -4,24 +4,37 @@ import com.returdev.authentication_service.model.UserTokens;
 
 /**
  * Interface for authentication services.
- * Provides methods for user login and logout functionality.
+ * This interface defines methods for handling user authentication,
+ * including login, logout, and token management.
  */
 public interface AuthService {
 
     /**
      * Authenticates a user and generates tokens.
+     * This method validates the user's credentials and returns
+     * access and refresh tokens for authenticated sessions.
      *
      * @param email the email of the user
      * @param password the password of the user
-     * @return a UserTokens object containing the access and refresh tokens
+     * @return a `UserTokens` object containing the access and refresh tokens
      */
     UserTokens login(String email, String password);
 
     /**
-     * Logs out a user by invalidating their session or tokens.
+     * Logs out a user by invalidating the refresh token.
+     * This method ensures the user's session is terminated securely.
      *
-     * @param email the email of the user to log out
+     * @param refreshToken the refresh token to be invalidated
      */
-    void logout(String email);
+    void logout(String refreshToken);
+
+    /**
+     * Provides a new access token using a valid refresh token.
+     * This method generates a fresh access token for continued authentication.
+     *
+     * @param refreshToken the refresh token used to generate a new access token
+     * @return a new access token as a `String`
+     */
+    String provideNewAccessToken(String refreshToken);
 
 }
